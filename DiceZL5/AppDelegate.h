d275fdd19adc2a438a4813c7c0b8541d8692e707
Jesus Magana <maganazero5@gmail.com>
2014-08-09 19:18:17 -0700

    Initial Commit

---
+++ DiceZL5/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DiceZL5
+//
+//  Created by Jesus Magana on 8/9/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DiceZL5/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DiceZL5
+//
+//  Created by Jesus Magana on 8/9/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
